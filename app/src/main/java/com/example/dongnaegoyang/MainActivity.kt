f75ab7aa0e641cd1d61cc8188b0da09f4381ab53
minWachya <alal7272@naver.com>
2022-02-16 23:15:32 +0900

    first commit

---
+++ app/src/main/java/com/example/dongnaegoyang/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dongnaegoyang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
