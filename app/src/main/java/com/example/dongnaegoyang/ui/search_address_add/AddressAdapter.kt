1cd82bd6dfd8440ce35824b3972ff96b2dfd1299
minyoung <minwachya@gmail.com>
2022-10-04 11:38:20 +0900

    [feat] 고양이 추가 2단계-주소 검색 후 받아오는 기능 추가(#14)

--- app/src/main/java/com/example/dongnaegoyang/ui/search_address_add/AddressAdapter.kt
+++ app/src/main/java/com/example/dongnaegoyang/ui/search_address_add/AddressAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.dongnaegoyang.ui.search_address_add
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -8,37 +9,47 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.dongnaegoyang.data.local.Address
 import com.example.dongnaegoyang.databinding.ItemAddressAddBinding
 
-class AddressAdapter: ListAdapter<Address, AddressAdapter.AddressViewHolder>(AddressDiffCallback()) {
-        private lateinit var binding: ItemAddressAddBinding
+class AddressAdapter(val context: Context) :
+    ListAdapter<Address, AddressAdapter.AddressViewHolder>(AddressDiffCallback()) {
+    private lateinit var binding: ItemAddressAddBinding
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressViewHolder {
-            binding = ItemAddressAddBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return AddressViewHolder(binding)
-        }
+    // Adapter 값 리턴하기 위한 인터페이스 객체
+    private lateinit var mCallback: SelectAddressInterface
 
-        override fun onBindViewHolder(holder: AddressViewHolder, position: Int) {
-            holder.bind(getItem(position), position)
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressViewHolder {
+        mCallback = context as SelectAddressInterface
 
-        inner class AddressViewHolder(private val binding: ItemAddressAddBinding): RecyclerView.ViewHolder(binding.root) {
-            fun bind(address: Address, position: Int) {
-                binding.address = address
-                binding.executePendingBindings()
+        binding = ItemAddressAddBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AddressViewHolder(binding)
+    }
 
-                binding.constraintLayout.setOnClickListener {
-                    // 리턴할 값 넣기!!
-//                    mCallback.onSelectedImage(position)
-                }
+    override fun onBindViewHolder(holder: AddressViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class AddressViewHolder(private val binding: ItemAddressAddBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(address: Address) {
+            binding.address = address
+            binding.executePendingBindings()
+
+            binding.constraintLayout.setOnClickListener {
+                // 리턴할 값 넣기!!
+                mCallback.onSelectedAddress(address.addressInfo.address1,
+                    address.addressInfo.address2,
+                    address.addressInfo.address3)
             }
         }
-
     }
 
-    class AddressDiffCallback: DiffUtil.ItemCallback<Address>() {
-        override fun areItemsTheSame(oldItem: Address, newItem: Address): Boolean {
-            return oldItem.addressName == newItem.addressName
-        }
-        override fun areContentsTheSame(oldItem: Address, newItem: Address): Boolean {
-            return oldItem == newItem
-        }
+}
+
+class AddressDiffCallback : DiffUtil.ItemCallback<Address>() {
+    override fun areItemsTheSame(oldItem: Address, newItem: Address): Boolean {
+        return oldItem.addressName == newItem.addressName
     }
+
+    override fun areContentsTheSame(oldItem: Address, newItem: Address): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/com/example/dongnaegoyang/ui/search_address_add/SearchAddressActivity.kt
+++ app/src/main/java/com/example/dongnaegoyang/ui/search_address_add/SearchAddressActivity.kt
@@ -1,7 +1,7 @@
 package com.example.dongnaegoyang.ui.search_address_add
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -10,10 +10,13 @@ import com.example.dongnaegoyang.databinding.ActivitySearchAddressAddBinding
 import com.example.dongnaegoyang.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
+
 private const val TAG = "mmmAddressSearchActivityAdd"
 
 @AndroidEntryPoint
-class SearchAddressActivity : BaseActivity<ActivitySearchAddressAddBinding>(R.layout.activity_search_address_add) {
+class SearchAddressActivity :
+    BaseActivity<ActivitySearchAddressAddBinding>(R.layout.activity_search_address_add),
+    SelectAddressInterface{
     private val viewModel: SearchAddressViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,17 +25,6 @@ class SearchAddressActivity : BaseActivity<ActivitySearchAddressAddBinding>(R.la
         setToolbar()
         searchAddress()
         setObserveAddress()
-    }
-
-    private fun setObserveAddress() {
-        viewModel.addressResponse.observe(this) { response ->
-            binding.rvAddress.adapter =
-                AddressAdapter().apply {
-                    submitList(response.documents)
-                }
-            Log.d("mmm", response.toString())
-        }
-
     }
 
     // 툴바 달기
@@ -52,11 +44,30 @@ class SearchAddressActivity : BaseActivity<ActivitySearchAddressAddBinding>(R.la
         }
     }
 
+    private fun setObserveAddress() {
+        viewModel.addressResponse.observe(this) { response ->
+            binding.rvAddress.adapter =
+                AddressAdapter(this).apply {
+                    submitList(response.documents)
+                }
+        }
+
+    }
+
     // 툴바에서 뒤로가기 버튼 클릭 시
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             android.R.id.home -> finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSelectedAddress(address1: String, address2: String, address3: String) {
+        val intent = Intent()
+        intent.putExtra("address1", address1)
+        intent.putExtra("address2", address2)
+        intent.putExtra("address3", address3)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
